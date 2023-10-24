5f20197170ef5810bc5e8a21b331633efb81f28e
Leo <leonardosilva01107@gmail.com>
2023-10-23 21:29:51 -0300

    Content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,17 @@
+export const dataFake =[
+    {
+        "id":"1",
+        "title":"Homem de Ferro em 3D",
+        "description":"marvel anuncia um novo filme do homem de ferro, confira",
+        "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/80C64C0B63382CD3ED2653356125F275F63D036028A77D38DC3286AD851A6833/scale?width=1200&aspectRatio=1.78&format=jpeg"
+    },
+
+    {
+        "id":"2",
+        "title":"oi",
+        "description":"oi oi",
+        "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+    }
+
+]
+    
